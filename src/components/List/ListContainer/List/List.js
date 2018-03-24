0483d8b1d37da9d5d25547a1b9620f924a71f1cf
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2018-03-24 17:32:54 +0700

    :fire: Re Structures App

--- src/components/List/ListContainer/List/List.js
+++ src/components/List/ListContainer/List/List.js
@@ -4,7 +4,48 @@ import React, { Component } from 'react'
 class List extends Component {
   render () {
     return (
+      <div>
+        <div className='page-header'>
+          <h2>0 Card(s)</h2>
+        </div>
+        <div className='btn-group btn-group-md' role='group' aria-label='filters'>
+          <a href='' className='btn btn-primary'>All</a>
+          <a href='' className='btn btn-default'>General</a>
+          <a href='' className='btn btn-default'>Code</a>
+          <a href='' className='btn btn-default'>Known</a>
+          <a href='' className='btn btn-default'>Unknown</a>
+        </div>
 
+        <br />
+        <br />
+
+        <table className='table table-bordered'>
+          <tbody>
+            <tr>
+              <td>
+                <a href='/edit' className='btn btn-xs btn-primary'>
+                  <i className='fa fa-pencil' aria-hidden='true' /> Edit
+                </a>
+                <a href='/delete' className='btn btn-xs btn-primary'>
+                  <i className='fa fa-trash' aria-hidden='true' /> Delete
+                </a>
+              </td>
+              <td className='cardContent'>
+                <h4>
+                  From
+                </h4>
+
+                <pre>
+                  <code>
+                    Back
+                  </code>
+                </pre>
+
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
--- src/components/Memorize/MemorizeContainer/Memorize/Memorize.js
+++ src/components/Memorize/MemorizeContainer/Memorize/Memorize.js
@@ -4,7 +4,89 @@ import React, { Component } from 'react'
 class Memorize extends Component {
   render () {
     return (
+      <div>
 
+        {/* Card Type */}
+        <div className='row'>
+          <div className='col-xs-12 text-center'>
+            <div className='btn-group btn-group-lg' role='group' aria-label='card type'>
+              <a href='general' className='btn btn-primary'>General</a>
+              <a href='code' className='btn btn-default'>Code</a>
+            </div>
+          </div>
+        </div>
+
+        <hr />
+
+        {/* Card */}
+        <div className='row memorizePanel'>
+          <div className='col-xs-8 col-xs-offset-2'>
+            <div className='panel panel-default cardFront'>
+              <div className='panel-body'>
+                <div className='alignContainer'>
+                  <div className='alignMiddle frontText'>
+                    <h3 className='text-center'>card.front</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='panel panel-primary cardBack'>
+              <div className='panel-body'>
+                <div className='alignContainer'>
+                  <div className='alignMiddle frontText'>
+
+                    <div className='text-center largerText'>
+                      card.back
+                    </div>
+
+                    <pre>
+                      <code>
+                        card.back
+                      </code>
+                    </pre>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Buttons */}
+        <div className='row'>
+          <div className='col-xs-12 text-center'>
+            <a href='' className='btn btn-primary btn-lg flipCard'>
+              <i className='fa fa-exchange' />
+              Flip Card
+            </a>
+            &nbsp;
+            &nbsp;
+            <a href='mark_known' className='btn btn-success btn-lg'>
+              <i className='fa fa-check' />
+              I Know It
+            </a>
+            &nbsp;
+            &nbsp;
+            <a href='card_type' className='btn btn-primary btn-lg'>
+              Next Card
+              <i className='fa fa-arrow-right' />
+            </a>
+          </div>
+        </div>
+
+        {/* Bookmark */}
+        <div className='row'>
+          <div className='col-xs-12 text-center'>
+            <br />
+            <br />
+            <br />
+            <a href='' className='btn btn-default btn-sm'>
+              <i className='fa fa-bookmark' />
+              bookmark this card (# card.id )
+            </a>
+          </div>
+        </div>
+      </div>
     )
   }
 }
