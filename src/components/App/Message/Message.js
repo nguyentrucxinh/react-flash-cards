05b94a4f352bd10c52cef83446cbfa2eae66bdb3
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2018-03-24 17:11:53 +0700

    :fire: Re Structures App

---
+++ src/components/App/Message/Message.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Message = () => (
+  <div className='alert alert-success' role='alert'>
+    message
+  </div>
+)
+
+export default Message
---
+++ src/components/App/Nav/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Nav = () => (
+  <nav className='navbar navbar-default'>
+    <div className='navbar-header'>
+      <a className='navbar-brand' href='../index.html'>CS Flash Cards</a>
+
+      <ul className='nav navbar-nav navbar-right'>
+
+        <li><Link to='/login'>Login</Link></li>
+        <li><Link to='/memorize'>Memorize</Link></li>
+        <li><Link to='/list'>List</Link></li>
+        <li><Link to='/form'>Add/Edit</Link></li>
+        <li>&nbsp;&nbsp;&nbsp;&nbsp;</li>
+        <li><Link to='/logout'>Log out</Link></li>
+
+      </ul>
+    </div>
+  </nav>
+)
+
+export default Nav
