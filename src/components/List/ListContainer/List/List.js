ca052508f61c0160ffad6ba31bf216fb26fafdc7
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2018-03-25 14:03:53 +0700

    :sparkles: Add modal delete

--- src/components/List/ListContainer/List/List.js
+++ src/components/List/ListContainer/List/List.js
@@ -26,7 +26,7 @@ class List extends Component {
             <i className='fa fa-pencil' aria-hidden='true' /> Edit
           </a>
           <br />
-          <a onClick={this.handleButtonDelete} className='btn btn-xs btn-danger'>
+          <a className='btn btn-xs btn-danger' data-toggle='modal' data-target='#deleteModal'>
             <i className='fa fa-trash' aria-hidden='true' /> Delete
           </a>
         </td>
@@ -83,6 +83,25 @@ class List extends Component {
             }
           </tbody>
         </table>
+
+        {/* Modal */}
+        <div className='modal fade' id='deleteModal' role='dialog'>
+          <div className='modal-dialog modal-sm'>
+            <div className='modal-content'>
+              <div className='modal-header'>
+                <button type='button' className='close' data-dismiss='modal'>&times;</button>
+                <h4 className='modal-title'>Are you sure?</h4>
+              </div>
+              <div className='modal-body'>
+                <p>This is a small modal.</p>
+              </div>
+              <div className='modal-footer'>
+                <button type='button' className='btn btn-danger' data-dismiss='modal'>Delete</button>
+                <button type='button' className='btn btn-default' data-dismiss='modal'>Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
