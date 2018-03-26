5ad901f58fc597e456f8d8f0a4ec271cbc6d2564
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2018-03-26 20:56:58 +0700

    :sparkles: Update description for Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => (
   <div className='row'>
     <div className='panel panel-default'>
       <div className='panel-body'>
         <div className='text-center'>
-          <img alt='Nguyen Truc Xinh' src='http://nguyentrucxinh.github.io/img/avatar.png' className='img-circle' width='100' height='100' />
-          <h3 className='text-success'><strong>Nguyen Truc Xinh</strong></h3>
-          <h4 className='text-default'>Full-stack JavaScript Developer</h4>
+          <img alt='Nguyen Truc Xinh' src='http://placehold.it/100x100' className='img-circle' width='100' height='100' />
+          <h3 className='text-success'><strong>Spaced Repetition Software (SRS)</strong></h3>
+          <h4 className='text-default'>
+            SRS is essentially digital flashcard software.
+            <br />
+            But unlike other flashcard software, it tries to schedule card reviews at efficient intervals.
+          </h4>
         </div>
         <p className='text-center'>
-          <strong>JavaScript</strong> junkie & <strong>Java</strong> lover. <br />
-          Build the great tools for the convenience of the human. <br />
-          Favorite stacks: <strong>Node.js/React/MongoDB - JavaEE/Spring/Hibernate</strong>. <br />
-          Mostly.
+          The theory behind spaced repetition is that humans rarely memorize a new fact all at once.
+          <br />
+          The first time we see a fact, we may only remember for a day or two.
+          <br />
+          But if we force ourselves to remember or review that fact before we forget it, we'll remember it longer the next time.
+          <br />
+          So if we review something at increasing intervals, we'll have a good chance of remembering it indefinitely.
+          <br />
+          It's possible to do this by hand, using index cards and some sort of scheduling system, but most people use special flashcard software with built-in support for scheduling cards.
+          <br />
+          See also <a href='https://en.wikipedia.org/wiki/Spaced_repetition' target='_blank' rel='noopener noreferrer'>Wikipedia: Spaced repetition.</a>
         </p>
-        <a className='btn btn-success center-block' href='https://www.linkedin.com/in/nguyentrucxinh/' target='_blank'>View More</a>
+        <Link to='/memorize' className='btn btn-success center-block'>View More</Link>
       </div>
     </div>
   </div>
